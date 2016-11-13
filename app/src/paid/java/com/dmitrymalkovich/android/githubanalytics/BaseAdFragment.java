273638100956cc71364110d933096768c89e7682
Dmitry Malkovich <dima.malkovich@gmail.com>
2016-11-14 00:18:14 +0300

    Added screenshots, Signed APK

---
+++ app/src/paid/java/com/dmitrymalkovich/android/githubanalytics/BaseAdFragment.java
@@ -0,0 +1,13 @@
+package com.dmitrymalkovich.android.githubanalytics.traffic;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class BaseAdFragment extends Fragment {
+}
