bad1369de620245a6a85c0c8cf400b171e657e02
Dmitry Malkovich <dima.malkovich@gmail.com>
2017-05-17 00:33:16 +0300

    GITO-21 As a developer, I'd like to use Travis CI

--- github-api/src/main/java/com/dmitrymalkovich/android/githubapi/core/GithubService.java
+++ github-api/src/main/java/com/dmitrymalkovich/android/githubapi/core/GithubService.java
@@ -39,8 +39,8 @@ import retrofit2.http.Query;
  */
 public interface GithubService {
 
-    String clientId = "";
-    String clientSecret = "";
+    String clientId = "e0bcea9f880637cd0e7a";
+    String clientSecret = "3ab8f9e3db083ca80211a48ef13e870b8e6fb78b";
     String redirectUri = "githubanalytics://auth";
 
     @FormUrlEncoded
